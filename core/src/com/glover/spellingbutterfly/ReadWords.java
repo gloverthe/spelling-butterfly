639ec1614c0aecc5b384264729137ed44863239a
gloverthe <glover.the@gmail.com>
2021-03-25 20:07:37 +0000

    fixed the readwords so it is cross platform

--- core/src/com/glover/spellingbutterfly/ReadWords.java
+++ core/src/com/glover/spellingbutterfly/ReadWords.java
@@ -1,5 +1,7 @@
 package com.glover.spellingbutterfly;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.math.MathUtils;
 
 import java.io.BufferedReader;
@@ -12,9 +14,9 @@ public class ReadWords {
 
     public static ArrayList<String> wordListArray(String filename) {
         ArrayList<String> wordListArray = new ArrayList<>();
-
+        FileHandle file = Gdx.files.internal("WordLists/" + filename + ".txt");
         try {
-            BufferedReader br = new BufferedReader(new FileReader("WordLists/" + filename + ".txt"));
+            BufferedReader br = new BufferedReader(file.reader());
             while (br.ready()) {
                 wordListArray.add(br.readLine());
             }
